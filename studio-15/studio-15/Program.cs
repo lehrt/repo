19059d4073351d68d39757dc243f94e6261ecf2c
rashida richmond <richmond1895@gmail.com>
2021-06-22 16:53:13 -0400

    Rashida's comment!

--- studio-15/studio-15/Program.cs
+++ studio-15/studio-15/Program.cs
@@ -12,6 +12,7 @@ namespace studio_15
             Console.WriteLine("Hey this is actually kinda cool. Extra line");
             Console.BackgroundColor = ConsoleColor.Yellow;
             Console.WriteLine("It says it's yellow but it still looks the same to me. Adding another sentence.");
+            Console.WriteLine("Girls rock!!");
         }
         
 
