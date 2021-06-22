7b4aa94256004ffec09b55828449272cc0535a6a
rashida richmond <richmond1895@gmail.com>
2021-06-22 17:24:29 -0400

    merge conflict

--- studio-15/studio-15/Program.cs
+++ studio-15/studio-15/Program.cs
@@ -7,10 +7,10 @@ namespace studio_15
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Changed");
+            Console.WriteLine("WwWweeeee");
             Console.WriteLine("Nice to finally be here!");
             Console.WriteLine("Hey this is actually kinda cool. Extra line");
-            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.BackgroundColor = ConsoleColor.Green;
             Console.WriteLine("It says it's yellow but it still looks the same to me. Adding another sentence.");
             Console.WriteLine("Girls rock!!");
         }
