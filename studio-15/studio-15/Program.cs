b81d46b9a109f0adac65c834166621e3c2578920
theresa.lehr17@gmail.com <theresa.lehr17@gmail.com>
2021-06-22 11:50:33 -0500

    I made a cw after your color change.

--- studio-15/studio-15/Program.cs
+++ studio-15/studio-15/Program.cs
@@ -10,6 +10,7 @@ namespace studio_15
             Console.WriteLine("Nice to finally be here!");
             Console.WriteLine("Hey this is actually kinda cool. Extra line");
             Console.BackgroundColor = ConsoleColor.Yellow
+            Console.WriteLine("It says it's yellow but it still looks the same to me.")
         }
         
 
