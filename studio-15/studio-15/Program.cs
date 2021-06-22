bcde3b5dd85444e2ee1d8cd9ef227f7dbb780093
Chazmen Smith <chazmensmith@Chazmen-2.local>
2021-06-22 16:06:54 -0400

    changed words

--- studio-15/studio-15/Program.cs
+++ studio-15/studio-15/Program.cs
@@ -6,7 +6,7 @@ namespace studio_15
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello everyone!");
+            Console.WriteLine("Changed");
             Console.WriteLine("Nice to finally be here!");
             Console.WriteLine("Hey this is actually kinda cool. Extra line");    
         }
